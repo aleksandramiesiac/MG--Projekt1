c792a8b16b7a0937b4ebfcd393cf9853ce948c09
Agata Paldyna <paldyna.agata@gmail.com>
2019-03-02 21:27:06 +0100

    Minor fixes in classes methods

--- neural_network.py
+++ neural_network.py
@@ -16,19 +16,20 @@ class NeuralNetwork:
 
     def feedforward(self):
         self.layers[0] = self.layers[0](np.dot(self.input_vector, self.layers[0].weight_vector))
-        for i in range(1,self.layers.len()):
+        for i in range(1, len(self.layers)):
             self.layers[i] = self.layers[i].activation_function(np.dot(self.layers[i-1], self.layers[i].weight_vector))
-            
+    
 
     def append_layer(self,new_layer):
         self.layers.append(new_layer)
 
 
-class Layer:
-    def __init__(self,input_size,layer_size, number_of_function):
 
+class Layer:
+    def __init__(self, input_size, layer_size, number_of_function):
         self.weight_vector = np.random.randn(input_size, layer_size)
-        self.activation_function = activation_function(number_of_function)
+        self.number_of_activation_function = number_of_function
+    
 
     def activation_function(self, number_of_function):
         """
@@ -41,20 +42,20 @@ class Layer:
             6.Sigmoidalna funkcja bipolarna (tangens hiperboliczny) 
             7.Funkcja Gaussa 
         """
-        if number_of_function ==1:
+        if self.number_of_activation_function == 1:
             return (1/2)*self.weight_vector
 
-        elif number_of_function ==2:
+        elif self.number_of_activation_function == 2:
             wektor=[]
             for x in self.weight_vector:
                 if x < -1 :
                     wektor.append(-1)
-                elif x >1:
+                elif x > 1:
                     wektor.append(1)
                 else:
                     wektor.append(x)
 
-        elif number_of_function ==3:
+        elif self.number_of_activation_function == 3:
             wektor=[]
             for x in self.weight_vector:
                 if x < 0 :
@@ -62,7 +63,7 @@ class Layer:
                 else:
                     wektor.append(1)
         
-        elif number_of_function ==4:
+        elif self.number_of_activation_function == 4:
             wektor=[]
             for x in self.weight_vector:
                 if x < 0 :
@@ -70,16 +71,15 @@ class Layer:
                 else:
                     wektor.append(1)
 
-        elif number_of_function ==5:      
+        elif self.number_of_activation_function == 5:      
             return 1/(1+np.exp(-self.weight_vector))
 
-        elif number_of_function ==6:
+        elif self.number_of_activation_function == 6:
             return (1-np.exp(-self.weight_vector))/(1+np.exp(-self.weight_vector))
 
-        elif number_of_function ==7:
+        elif self.number_of_activation_function == 7:
             return np.exp((-(self.weight_vector)**2)/2)
         
         else:
             raise Exception("Błędny numer funkcji aktywacji! Wybierz od 1-7")
 
-            
