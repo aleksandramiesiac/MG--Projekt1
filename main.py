c1452f66d0de093d664600ce677932a72eee26ee
kasia <kasia.spalinska@gmail.com>
2019-03-06 15:40:00 +0100

    wczytywanie danych

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ Plik generujacy siec neuronowa
 """
 
 import neural_network as nn
+import pandas as pd
+
+# Wczytywanie danych treningowych i testowych
+
+# klasyfikacja:
+train_set = pd.read_csv('Dane/Classification/data.circles.train.500.csv')
+test_set = pd.read_csv('Dane/Classification/data.circles.test.500.csv')
+
+train_set_X = train_set[['x','y']]
+train_set_y = train_set['cls']
 
 
 # Wczytywanie pliku konfiguracyjnego i tworzenie slownika z danymi konfiguracyjnymi
@@ -16,4 +26,11 @@ with open("configuration_file.txt", "r") as conf_file:
 
 
 # Tworzenie sieci neuronowej
-# NN = nn.NeuralNetwork(x, y, config)
+NN = nn.NeuralNetwork(train_set_X, train_set_y, config)
+
+# proces uczenia sieci
+
+NN.train()
+
+NN.predict()
+
